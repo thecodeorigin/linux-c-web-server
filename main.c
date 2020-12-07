8e4bdb7f2ede410302357b87c5cc9f82c17f77cc
Le Viet Long <vilagondk@gmail.com>
2020-12-07 23:28:14 +0700

    Finish

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-// INCLUDES 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,15 +8,13 @@
 #include <arpa/inet.h>
 #include <time.h> 
 
-// DEFINES 
+
 #define MAX_CONNECTIONS 100
 #define PATH_MAX 4096 
 
-// GLOBAL SEC. 
 char ROOT[PATH_MAX];
-int BUFFER_SIZE = 51200; // 50Kb = default page table size
+int BUFFER_SIZE = 51200; // 50KB = Kich thuoc page
 int PORT = 3000;
-// int PORT = 31337;
 
 char *responses[] = 
 {
@@ -28,8 +25,6 @@ char *responses[] =
 };	
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-///////////////////////////////////////////////////////////////////////////////////////////////
-
 
 void getClientAddr(int sock_fd)
 {
@@ -43,8 +38,6 @@ void getClientAddr(int sock_fd)
 
 	printf("Client connected. [IP addr: %s]\n",clientip);
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
-
 
 void handle_request(char *client_request, int sock_fd)
 {
@@ -58,23 +51,17 @@ void handle_request(char *client_request, int sock_fd)
 	reqline[2] = malloc(BUFFER_SIZE);
 	
 
-	// parser the request received from client
+	// Parse request nhan tu client
 	printf("\033[1;92mPARSING REQUEST...\033[0m\n");
-	// printf("%s",client_request);
 
-	// reqline[0] = http method
-	// reqline[1] = file required
-	// reqline[2] = protocol version
 	reqline[0]	= strtok(client_request," ");
 	reqline[1] 	= strtok(NULL," ");
 	reqline[2] 	= strtok(NULL,"\r\n");
 	
 
-	// ONLY accepts HTTP/1.0 and HTTP/1.1 request headers from client
-	// Persistent connection is an opt-in feature, anyway 
+	// Chi chap nhan HTTP/1.0 and HTTP/1.1 request headers tu client
 	if (strncmp(reqline[2], "HTTP/1.0", 8)!=0 && strncmp(reqline[2], "HTTP/1.1", 8)!=0 )
 	{
-
 		send(sock_fd, responses[2], strlen(responses[2]), 0);
 		return;
 	}
@@ -85,17 +72,14 @@ void handle_request(char *client_request, int sock_fd)
 	printf("reqline[2]	= %s \n",reqline[2]);
 	
 
-	/*	****************************** HANDLE HTTP METHODS ****************************** */
-	//	GET METHOD
+	/*	****************************** XU LY HTTP ****************************** */
+	//	Phuong thuc GET
 	if(strcmp(reqline[0],"GET") == 0)
 	{		
-		if ( strncmp(reqline[1], "/\0", 2)==0 )
-			// If no file is specified, index.html will be opened by default   
-			// strcpy(reqline[1],"/cat.jpg");
+		if (strncmp(reqline[1], "/\0", 2)==0)
+			// Neu khong co tep duoc chi dinh, index.html se duoc mo mac dinh
 			strcpy(reqline[1],"/www/index.html");
 
-	
-		// printf("ROOT = %s \n",ROOT);	
 		strcpy(path, ROOT);
 		strcpy(&path[strlen(ROOT)], reqline[1]);
 		printf("File Required = %s\n", path);
@@ -104,19 +88,17 @@ void handle_request(char *client_request, int sock_fd)
 		bytes_read = 0;
 		bytes_write = 0;
 		temp = 0;
-		if (fp = fopen(path, "r" ))	//FILE FOUND
+		if (fp = fopen(path, "r" ))	// Mo File
 		{
-			fd = fileno(fp);
+			fd = fileno(fp); // Lay File Descriptor
 
-			c = fgetc(fp);
+			c = fgetc(fp); // Doc File
 			while(c != EOF)
 			{
-				// data_to_send[bytes_read] = c;
 				bytes_read++;
 				c = fgetc(fp);
 			}
-			// data_to_send[bytes_read] = '\0';
-			rewind(fp);
+			rewind(fp); // Dua con tro ve dau File
 
 			char str[4];
 			sprintf(str, "%d", bytes_read);
@@ -125,9 +107,7 @@ void handle_request(char *client_request, int sock_fd)
 			send(sock_fd, "Content-Length: ", strlen("Content-Length: "), 0);
 			send(sock_fd, str, strlen(str), 0);
 			send(sock_fd, "\n", 1, 0);
-			// send(sock_fd, "Keep-Alive: max=3\n", strlen("Keep-Alive: max=3\n"), 0);
 			send(sock_fd, "Keep-Alive: timeout=3\n\n", strlen("Keep-Alive: timeout=3\n\n"), 0);
-			// send(sock_fd, "\n\n", 2, 0);
 
 			
 			bytes_read = 0;
@@ -137,7 +117,6 @@ void handle_request(char *client_request, int sock_fd)
 				bytes_read += temp;
 				bytes_write += write(sock_fd, data_to_send, temp);
 			}
-			// bytes_write += write(sock_fd, data_to_send, bytes_read);
 			fclose(fp);	
 			
 		
@@ -146,17 +125,13 @@ void handle_request(char *client_request, int sock_fd)
 		}
 		else
 		{
-			write(sock_fd, responses[1], strlen(responses[1])); //FILE NOT FOUND
+			write(sock_fd, responses[1], strlen(responses[1])); // Khong tim thay File
 			printf("File not found.\n");
 			return;
 		}
 	}
-	
-
 	return;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
-
 
 void * respond(void *arg)
 {
@@ -164,11 +139,11 @@ void * respond(void *arg)
 	char clientRequest[BUFFER_SIZE], singleChar[1];
 	clock_t t;
 	
-	// get socket FD 
+	// Tao socket_fd
 	sock_fd = *((int *)arg);
 	printf("#[sock_fd]: %d\n", sock_fd);
 	
-	// initialize consumer variables
+	// Khoi tao cac bien
 	memset((void*)clientRequest, (int)'\0', BUFFER_SIZE);
 	singleChar[0] = '\0';
 	bytes_read = 0;
@@ -176,24 +151,19 @@ void * respond(void *arg)
 	total_requests = 0;
 	i = 0, j = 0;
 
-	// read data from client socket until find 2x CRLF's, or the timeout param value has achieved their limit
-	// default timeout set to 3 sec's
+	// Doc data tu client socket cho den khi tim thay 2 ky tu CRLF (\r\n\r\n) hoac het thoi gian timeout (default=3s)
 	while (1)
 	{	
-		
 		if(j == 0)
 			printf("\n\033[1;35mRECEIVING HEADERS...\033[0m\n");
-		
-		// start timeout mark
-		// t = clock();
-	
+
 		singleByte = recv(sock_fd, singleChar, 1, 0);
-		if (singleByte < 0) // receive error
+		if (singleByte < 0) // Loi nhan du lieu
 		{
 			fprintf(stderr,"Error receiving data from client.\n");
 			break;
 		}
-		else if (singleByte == 0) // socket closes
+		else if (singleByte == 0) // Socket bi dong
 		{
 			fprintf(stderr,"Client disconnected unexpectedly.\n");
 			break;
@@ -201,21 +171,18 @@ void * respond(void *arg)
 		bytes_read++;
 
 
-		// clients header already received here
-		// verifys '\r\n\r\n' sequence
-		// if(bytes_read == )
+		// Client Header da duoc nhan
+		// '\r\n\r\n' = chuoi ket thuc
 		if(singleChar[0] == '\n' && clientRequest[i - 2] == '\n')
 		{						
 		
 			clientRequest[i] = singleChar[0];
 			printf("%c",clientRequest[i]);
-			// printf("#[bytes_read] CLIENT REQUEST HEADER = %d\n", bytes_read);
 
-
-			// handle request type properly
+			// Xu ly request
 			handle_request(clientRequest, sock_fd);
 			
-			// prepare to a new request header
+			// Reset lai bien de nhan header moi
 			memset((void*)clientRequest, (int)'\0', BUFFER_SIZE);
 			bytes_read = 0;
 			crlf_count = 0;
@@ -223,20 +190,15 @@ void * respond(void *arg)
 			j = 0;
 			continue;		
 		}
-
-		else if(singleChar[0] == '\n' && crlf_count == 0)
-		{
+		else if(singleChar[0] == '\n' && crlf_count == 0) { // Da tim thay ky tu bat dau
 			clientRequest[i] = singleChar[0];
 			printf("%c",clientRequest[i]);
 			crlf_count = 1;
 			j = 1;
 			i++;
 			continue;	
-		}
-
-		else
-		{
-			clientRequest[i] = singleChar[0];
+		} else {
+			clientRequest[i] = singleChar[0]; // Doc noi dung request
 			printf("%c",clientRequest[i]);
 			j = 1;
 			i++;
@@ -250,7 +212,6 @@ void * respond(void *arg)
 	pthread_exit(&sock_fd);
 	return NULL;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
@@ -259,17 +220,14 @@ int main(int argc, char *argv[])
     socklen_t addrlen = sizeof(struct sockaddr_in);
     int server_fd, sock_fd;
     int i = 0;
-	// int *ptr[MAX_CONNECTIONS];
 
-	
-	// If port number is not specified, set default http port
+	// Neu port khong duoc chi dinh thi su dung port mac dinh
 	PORT = ((argv[1] != (char *)' ') && (argc >= 2)) ? atoi(argv[1]) : PORT;
 
-	// ROOT to serve files
+	// Lay duong dan thu muc hien tai de chay file html
 	strcpy(ROOT,getenv("PWD"));
     
-    // Creating socket file descriptor
-    // if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    // Cai dat Server
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
@@ -280,21 +238,14 @@ int main(int argc, char *argv[])
     clientAddr.sin_addr.s_addr = INADDR_ANY;
     clientAddr.sin_port = htons(PORT);
 
-	// Attach socket to the clientAddr:port specified 
+	// Gan socket vao clientAddr:port da duoc xac dinh 
     if (bind(server_fd, (struct sockaddr *)&clientAddr, sizeof(clientAddr)) < 0)
     {
         perror("bind failed");
         exit(1);
     }
-	
-	// char content_length[BUFFER_SIZE];
-	// strcpy(content_length,"BANANA = ");
-	// int size = strlen(content_length);
-	// printf("\nPLANO = %s\n",content_length);
-	// // printf("\nPLANO = %s\n",strcat("Content-Length: ",(char *)itoa(strlen("adasdas"))));
-	// exit(0);
 
-	// run the server
+	// Lang nghe ket noi
     while (1)
     {
 		if (listen(server_fd, MAX_CONNECTIONS) < 0)
@@ -310,35 +261,19 @@ int main(int argc, char *argv[])
 			perror("Cannot accept connection");
 			exit(1);
         }
-		// else
-		// {
-		// 	int enable = 1;
-		// 	if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-    	// 		perror("setsockopt(SO_REUSEADDR) failed");
-		// }
-		
 	
-		// Get and prints client address 
+		// In dia chi cua Client
         getClientAddr(sock_fd);
 
-		// SEQUENCIAL 
-		// respond(&sock_fd);
-
-		// MULTITHREAD
-		// Create thread and handle request
+		// MultiThread
+		// Tao thread moi de xu ly request
         if (pthread_create(&threads[i % MAX_CONNECTIONS], NULL, &respond, &sock_fd) != 0)
 		{
 			fprintf(stderr, "error: Cannot create thread # %d\n", i);
 			break;	
 		}
 
-		//	BEGIN OF CRITICAL SECTION
-		// pthread_mutex_lock(&lock); 
-			
-		// pthread_mutex_unlock(&lock);
-		//	END OF CRITICAL SECTION
-	
-		// Wait thread finish their job
+		// Doi cho den khi thread hoan tat
 		if (pthread_join(threads[i % MAX_CONNECTIONS], NULL) != 0)
         {
 			fprintf(stderr, "error: Cannot join thread # %d\n", i);
@@ -350,12 +285,8 @@ int main(int argc, char *argv[])
 			printf("\033[1;31mTHREAD [# %d] HAS SUCCESSFULLY FINISHED THEIR JOB. \033[0m \n\n",i);
 		}
 
-		// increments # total conns
 		i++;
     }
-
-
-
+	
     return 0;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
